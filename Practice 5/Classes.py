f6069518fdc93d4000004be3a7d06dd4fcdeb019
Pavel Gromov <grom-razord@mail.ru>
2017-11-19 21:30:57 +1000

    Test version

--- Practice 5/Classes.py
+++ Practice 5/Classes.py
@@ -1,9 +1,10 @@
-"""
+""" Class
+
 3. Составить описание класса многочленов от одной переменной, задаваемых степенью многочлена и массивом коэффициентов.
-Предусмотреть методы для вы­числения значения многочлена для заданного аргумента, операции сложения,
+Предусмотреть методы для вычисления значения многочлена для заданного аргумента, операции сложения,
 вычитания и умножения многочленов с получением нового объекта-многочлена, вывод на экран описания многочлена.
 
-
+Pavel Gromov
 """
 
 
@@ -12,7 +13,7 @@ class Polynom():
     def __init__(self, coeffs):
         self.coeffs = coeffs
         self.DEGREE_MAX = len(coeffs) - 1
-        self.idx = 0
+        self.index1 = -1
 
     def __len__(self):
         count = 0
@@ -23,14 +24,16 @@ class Polynom():
         return count
 
     def __iter__(self):
-        return self
+        return iter(self.coeffs)
+        # return self
 
     def __next__(self):
         try:
-            result = self.coeffs[self.idx]
+            result = self.coeffs[self.index1]
         except IndexError:
             raise StopIteration
-        self.idx += 1
+
+        self.index1 -= 1
 
         return result
 
@@ -38,44 +41,66 @@ class Polynom():
         return self.coeffs
 
     def __str__(self):
-        expression = str()
-        degree = self.DEGREE_MAX
-
-        for element in self.coeffs:
-            if element >= 0: mark = "+"
-            else: mark = "-"
-
-            expression += "{} {}*x^{} ".format(mark, str(element), degree)
-            degree -= 1
-
-        return expression
+        # expression = str()
+        # degree = self.DEGREE_MAX
+        #
+        # for element in self.coeffs:
+        #     if element >= 0: mark = "+"
+        #     else: mark = ""
+        #
+        #     expression += "{} {}*x^{} ".format(mark, str(element), degree)
+        #     degree -= 1
+        #
+        # return expression
+        return str(self.coeffs)
 
     def __add__(self, other):
-        other = self.check_length(other)
+        first, second = self.check_length(other)
+        result = [0 for tmp in range(len(first))]
 
-        for idx, _ in enumerate(other):
+        for idx, val in enumerate(first):
+            result[idx] = val
+
+        for idx, _ in enumerate(second):
             last_idx = -(idx+1)
-            self.coeffs[last_idx] += other[last_idx]
+            result[last_idx] += second[last_idx]
 
-        return Polynom(self.coeffs)
+        return Polynom(result)
 
     def __sub__(self, other):
-        other = self.check_length(other)
+        first, second = self.check_length(other)
+        result = [0 for tmp in range(len(first))]
 
-        for idx, _ in enumerate(other):
+        for idx, val in enumerate(first):
+            result[idx] = val
+
+        for idx, _ in enumerate(second):
             last_idx = -(idx+1)
-            self.coeffs[last_idx] -= other[last_idx]
+            result[last_idx] -= second[last_idx]
 
-        return Polynom(self.coeffs)
+        return Polynom(result)
 
     def __mul__(self, other):
-        other = self.check_length(other)
+        first, second = self.check_length(other)
+        result = [0 for tmp in range(len(first) + len(second) - 1)]
+        shift = 0
 
-        for idx, _ in enumerate(other):
+        for idx, _ in enumerate(second):
             last_idx = -(idx+1)
-            self.coeffs[last_idx] *= other[last_idx]
+            last_val = second[last_idx]
+            tempList = [0 for tmp in range(len(first))]
 
-        return Polynom(self.coeffs)
+            for index, val in enumerate(first):
+                last_index = -(index+1)
+                tempList[last_index] = val * last_val
+
+            for j, val_tmp in enumerate(tempList):
+                last_idx_res = -(shift+j+1)
+                result[last_idx_res] += val_tmp
+
+            shift += 1
+
+        return Polynom(result)
 
     def __truediv__(self, other):
         other = self.check_length(other)
@@ -97,28 +122,30 @@ class Polynom():
         self.coeffs[index] = value
 
     def check_length(self, other):
-        if len(self.coeffs) < len(other):
-            self.coeffs, other = other, self.coeffs
-
-        return other
+        if len(self.coeffs) < len(other.coeffs):
+            return other, self.coeffs
+        else:
+            return self.coeffs, other
 
 
 def main():
-    a = [5, 7, 2, 4, -8]
-    b = [-8, 1, 13, 1]
-    c = [1, 2]
+    a = [1, 2, 3]
+    b = [4, 5]
 
     poly1 = Polynom(a)
     poly2 = Polynom(b)
-    poly3 = Polynom(c)
 
     print("Polynom 1 = ", poly1)
     print("Polynom 2 = ", poly2)
-    print("Polynom 3 = ", poly3)
 
-    summa = poly1 + poly2 * poly3
+    sum = poly1 + poly2
+    print("\nSumma: ", sum)
 
-    print("\nSumma: ", summa)
+    substract = poly1 - poly2
+    print("\nSubstract: ", substract)
+
+    multiply = poly1 * poly2
+    print("\nMultiply: ", multiply)
 
 
 if __name__ == "__main__":
