050200e78ac33a3a3624ba3a1740d0d9bf7cf7b4
Pavel Gromov <grom-razord@mail.ru>
2017-12-17 03:22:01 +1000

    Add task 1,2,3,4

--- Final 2/GItHub.py
+++ Final 2/GItHub.py
@@ -1,5 +1,6 @@
 import psycopg2 as db
 import urllib.request
+import urllib
 import json
 import datetime
 
@@ -16,42 +17,51 @@ def connect_to_database():
     try:
         connect = db.connect("dbname={} user={} host={} password={}"
                              .format(DBNAME, USER, HOST, PASSWORD))
-    except: #прописать исключение
+    except db.Error:
         print("I am unable to connect to the database")
+        exit()
 
     cur = connect.cursor()
 
 
 def insert_to_users(cur, connect, object):
-    cur.execute("""
-        INSERT INTO Users
-        (Login, URL, Repositories)
-        VALUES ('{}', '{}', '{}')""".format(object["login"],
-                                            object["url"],
-                                            object["repos_url"]))
-    connect.commit()
+    try:
+        cur.execute("""
+            INSERT INTO Users
+            (Login, URL, Repositories)
+            VALUES ('{}', '{}', '{}')""".format(object["login"],
+                                                object["url"],
+                                                object["repos_url"]))
+        connect.commit()
+    except db.Error:
+        print("Database Users already exists")
 
 
 def insert_to_repositories(cur, connect, object):
-    cur.execute("""
-        INSERT INTO Repositories
-        (Full_name, Name, Tag, Star, Update_time, Login_user, Tags_url)
-        VALUES ('{}', '{}', '{}', {}, '{}', '{}', '{}')
-        """.format(object["full_name"],
-                   object["name"],
-                   object["language"],
-                   object["stargazers_count"],
-                   object["updated_at"],
-                   object["owner"]["login"],
-                   object["languages_url"]))
-    connect.commit()
+    try:
+        cur.execute("""
+            INSERT INTO Repositories
+            (Full_name, Name, Tag, Star, Update_time, Login_user, Tags_url)
+            VALUES ('{}', '{}', '{}', {}, '{}', '{}', '{}')
+            """.format(object["full_name"],
+                       object["name"],
+                       object["language"],
+                       object["stargazers_count"],
+                       object["updated_at"],
+                       object["owner"]["login"],
+                       object["languages_url"]))
+        connect.commit()
+    except db.Error:
+        print("Database Repositories already exists")
 
 
 def update_to_users(cur, connect, object, count):
-    cur.execute("""UPDATE Users SET Count_of_repositories = {} 
-                   WHERE Login = '{}'""".format(count, object["login"]))
-    connect.commit()
-
+    try:
+        cur.execute("""UPDATE Users SET Count_of_repositories = {} 
+                       WHERE Login = '{}'""".format(count, object["login"]))
+        connect.commit()
+    except db.Error:
+        print("Current transaction is aborted")
 
 
 def create_query(url):
@@ -70,11 +80,9 @@ def create_query(url):
 
 
 def get_json_object(url):
-    # прописать исключения"""urllib.request.RequestError:"""
-
     try:
         response = urllib.request.urlopen(url)
-    except:
+    except urllib.request.URLError:
         print("Нет интернета!")
         exit()
     json_obj = json.loads(response.read().decode())
@@ -88,7 +96,18 @@ def show_task_1(tag):
         FROM Repositories
         WHERE Tag = '{}'
         """.format(tag))
+    connect.commit()
+    show_data()
 
+
+def show_task_2(first_date, second_date):
+    cur.execute("""
+        SELECT Full_name, Star, Update_time 
+        FROM Repositories
+        WHERE update_time
+        BETWEEN '{}' AND '{}'
+        ORDER BY Star DESC
+        """.format(first_date, second_date))
     connect.commit()
     show_data()
 
@@ -117,33 +136,30 @@ def show_task_4():
 
 def show_data():
     rows = cur.fetchall()
-
-    print("\nShow me the databases:\n")
+    print("Query result")
     for row in rows:
         print(row)
 
 
 def main():
     URL_USERS = "https://api.github.com/users"
+    URL_USERS_2 = "https://api.github.com/users?page=2&per_page=70"
     connect_to_database()
 
-    # case = input("Check number ")
-
-    # if (case == "0"):
-    #     create_query(URL_USERS)
-    # elif (case == "1"):
-    #     tag = input("Enter the tag ")
-    #     show_task_1(tag)
-    # elif (case == "3"):
-    #     show_task_3()
-    # elif(case == "4"):
-    #     show_task_4()
-
-    current_date = datetime.datetime.today()
-    week_ago = current_date.replace(day=current_date.day-7)
-
-    print(current_date)
-    print(week_ago)
+    case = input("Check number ")
+    if (case == "0"):
+        create_query(URL_USERS_2)
+    elif (case == "1"):
+        tag = input("Enter the tag ")
+        show_task_1(tag)
+    elif (case == "2"):
+        current_date = datetime.datetime.today()
+        week_ago = current_date.replace(day=current_date.day - 7)
+        show_task_2(week_ago, current_date)
+    elif (case == "3"):
+        show_task_3()
+    elif(case == "4"):
+        show_task_4()
 
 
 if __name__ == "__main__":
@@ -151,6 +167,7 @@ if __name__ == "__main__":
 
 """
 исклчения psycopg2.IntegrityError: duplicate key value violates unique constraint "users_pkey"
+psycopg2.OperationalError: FATAL:  database "GitHub1" does not exist
 
-
+psycopg2.Error
 """
