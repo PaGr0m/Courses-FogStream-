ea31c89f48610debf351f72ea6b112ec0fde815d
Pavel Gromov <grom-razord@mail.ru>
2018-01-14 23:27:59 +1000

    Redact html + css. Add dump

--- Practice_9/mysite/discography/admin.py
+++ Practice_9/mysite/discography/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from .models import Album, Genre, Musician, Song
 
 
+class DiscographyAdmin(admin.ModelAdmin):
+    list_display = ["song", "musician", "album", "genre"]
+    list_filter = ["musician", "album",]
+    search_fields = ["song",]
+
+    # list_display = [field.name for field in Song._meta.fields]
+
+    class Meta:
+        model = Song
+
+
 admin.site.register(Album)
 admin.site.register(Genre)
 admin.site.register(Musician)
-admin.site.register(Song)
+admin.site.register(Song, DiscographyAdmin)
+
+# admin.site.register(DiscographyAdmin)
